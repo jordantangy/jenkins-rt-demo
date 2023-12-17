aa91c758c56b2552882bd090b96e668e032729d6
jordantangy <jordantangy@gmai.com>
2023-12-17 18:30:33 +0200

    first commit

---
+++ src/main/java/com/example/jenkinsrtdemo/JenkinsRtDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsrtdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsRtDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsRtDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jenkinsrtdemo/JenkinsRtDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsrtdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsRtDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
